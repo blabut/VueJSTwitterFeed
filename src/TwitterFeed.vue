44b95aa315c28c0314183239f41a50f3375a0bad
paul <pazoulai@gmail.com>
2019-12-07 17:32:03 +0100

    first commit

---
+++ src/TwitterFeed.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    class="twitter-timeline"
+    :href="src"
+    :data-theme="dark ? 'dark' : 'light'"
+    :data-link-color="dataLinkColor"
+  ></a>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: "https://twitter.com/vuejs?ref_src=twsrc%5Etfw"
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    },
+    dataLinkColor: {
+      type: String,
+      default: "#2B7BB9"
+    }
+  },
+  created() {
+    let twitterFeed = document.createElement("script");
+    twitterFeed.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    document.head.appendChild(twitterFeed);
+  }
+};
+</script>
